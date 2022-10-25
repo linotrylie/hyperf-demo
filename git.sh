cd22115a2e076c5318d844ee582dc36d77f6b1bb
root <lld2669877481@163.com>
2022-10-25 12:45:26 +0800

    init

---
+++ git.sh
@@ -0,0 +1,7 @@
+o "# hyperf-demo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:linotrylie/hyperf-demo.git
+git push -u origin main
